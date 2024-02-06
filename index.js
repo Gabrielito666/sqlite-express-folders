e62db66badc81046537c872afca1c6aecac6e951
Gabrielito666 <galileo.fandango@gmail.com>
2024-02-06 12:27:22 -0300

    3.0.1 initialize debug

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ class SqliteExpressFolders{
         this.sqlite_express_instance = sqlite_express_instance ? sqlite_express_instance : new SqliteExpress(rootPath);
         this.lek_dir_object_instance = lek_dir_object_instance ? lek_dir_object_instance : new LekDirObject();
         this.routeFolder = path.isAbsolute( routeFolder ) ? routeFolder : path.resolve( this.rootPath, routeFolder );
-    }
-    async initialize(){
         this.lek_dir_object_tree = this.lek_dir_object_instance.createTree(this.routeFolder, this.rootPath);
         this.lek_dir_object_tree.setSpesificMode(['methods']);
         if(!this.extern_lek_dir_object_instance) this.lek_dir_object_instance.initialize();
+    }
+    async initialize(){
         this.tree = await processTree(await this.lek_dir_object_tree.tree, this);
     };
 };
