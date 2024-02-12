b41997e5d070d94429f8754bef39ab34119c7d03
Gabrielito666 <galileo.fandango@gmail.com>
2024-02-12 11:17:00 -0300

    4.0.2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const SqliteExpress = require('sqlite-express');
 const lekDirObject = require('lek-dir-object');
 const path = require('path');
 const processTree = require('./modules/processTree');
-const sqliteExpressFolders = async ({ sqlite_express, root, route, createTree }) => {
+const sqliteExpressFolders = ({ sqlite_express, root, route, createTree }) => {
 
     const rootPath = root ? root : (module.parent.filename ? path.dirname(module.parent.filename) : '.');
     const sqlite_express_instance = sqlite_express ? sqlite_express : new SqliteExpress(rootPath);
